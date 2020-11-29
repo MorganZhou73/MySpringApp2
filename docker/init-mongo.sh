1d01e1266088be5f5ef09eb11a78d21a4fab45b8
MorganZhou73 <maogenzhou@gmail.com>
2020-11-28 21:53:40 -0500

    add init-mongo.sh

--- docker/init-mongo.sh
+++ docker/init-mongo.sh
@@ -1,16 +1,17 @@
 echo ">>>>>>> trying to create database and users"
 if [ -n "${MONGO_INITDB_ROOT_USERNAME:-}" ] && [ -n "${MONGO_INITDB_ROOT_PASSWORD:-}" ] && [ -n "${MONGO_INITDB_DATABASE:-}" ]; then
-mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD<<EOF
-	var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
-    var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
-    var admin = db.getSiblingDB('admin');
-    admin.auth(rootUser, rootPassword);
-	
-	db=db.getSiblingDB('$MONGO_INITDB_DATABASE');
-    db.createUser({user: rootUser, pwd: rootPassword, roles: ["readWrite"]});
-	db.createCollection("person");
-EOF
-else
-echo "MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD and MONGO_INITDB_DATABASE must be provided. Some of these are missing, hence exiting database and user creation"
-    exit 403
+  mongo -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" <<-EOJS
+  var rootUser = "$MONGO_INITDB_ROOT_USERNAME";
+  var rootPassword = "$MONGO_INITDB_ROOT_PASSWORD";
+  var admin = db.getSiblingDB("admin");
+  admin.auth(rootUser, rootPassword);
+  
+  var dbUser = "$dbUser";
+  var dbPassword = "$dbPwd";
+  db=db.getSiblingDB("$MONGO_INITDB_DATABASE");
+  db.createUser({user: dbUser, pwd: dbPassword, roles: ["readWrite"]});
+  db.createCollection("person");
+EOJS
 fi
+
+echo "created $dbUser in database $MONGO_INITDB_DATABASE"
